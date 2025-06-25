d1ac0316d44c8a418054f99b097472b32789d224
patricianagy <nagy.patricia98@gmail.com>
2025-06-25 10:02:43 +0200

    deledted unnessesary function

--- siemens-homework/components/users/users.ts
+++ siemens-homework/components/users/users.ts
@@ -64,17 +64,3 @@ export type UserList = {
     version: string;
   };
 };
-
-export async function getUsers(): Promise<UserList> {
-  return fetch("https://randomuser.me/api/", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  }).then((res) => {
-    if (!res.ok) {
-      throw new Error(`HTTP error! status: ${res.status}`);
-    }
-    return res.json();
-  });
-}
